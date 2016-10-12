360556d9ca4ecef473c07cc1fdad8bc447d1c376
Geoff Maggi <gmaggi@pdx.edu>
2016-10-12 10:17:00 -0700

    New version

--- time.java
+++
@@ -1,9 +0,0 @@
-class time_info {
-  public time_info() {
-  }
-  
-  public String get() {
-    String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
-    return mydate;
-  }
-}
